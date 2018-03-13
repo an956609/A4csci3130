00b3ff753bbe8f4e4632ee094c9851140fbb1512
an956609 <an956609@dal.ca>
2018-03-13 19:46:20 -0300

    changed gradle build files so junit could be recognized, added a test class for the 4 CRUD operations

---
+++ app/src/androidTest/java/com/acme/a3csci3130/CRUDTest.java
@@ -0,0 +1,29 @@
+package com.acme.a3csci3130;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by agagne on 13/03/18.
+ */
+@RunWith(AndroidJUnit4.class)
+public class CRUDTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivity =
+            new ActivityTestRule<MainActivity>(MainActivity.class);
+    
+}
