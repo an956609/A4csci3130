7c4e95fe5060c14174d33f6b02ae99e9f70ca63e
an956609 <an956609@dal.ca>
2018-03-13 21:08:36 -0300

    Added espresso test for creating new business account

--- app/src/androidTest/java/com/acme/a3csci3130/CRUDTest.java
+++ app/src/androidTest/java/com/acme/a3csci3130/CRUDTest.java
@@ -8,6 +8,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -16,6 +17,8 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.Assert.assertEquals;
+import static org.hamcrest.Matchers.anything;
 
 /**
  * Created by agagne on 13/03/18.
@@ -25,5 +28,40 @@ public class CRUDTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivity =
             new ActivityTestRule<MainActivity>(MainActivity.class);
-    
+    @Test
+    public void addContact() throws Exception {
+        mActivity.getActivity();
+
+        onView(withId(R.id.submitButton)).perform(click());
+
+        onView(withId(R.id.name)).perform(typeText("Business Name"));
+        onView(withId(R.id.number)).perform(typeText("123 456-7890"));
+        onView(withId(R.id.business)).perform(typeText("Fish Monger"));
+        onView(withId(R.id.address)).perform(typeText("00 Anywhere"));
+        onView(withId(R.id.province)).perform(typeText("AB"));
+        onView(withId(R.id.submitButton)).perform(click());
+
+        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(0)
+                .onChildView(withId(android.R.id.text1))
+                .check(matches(withText("Business Name")));
+    }
+
+    @Test
+    public void readContact() throws Exception {
+        mActivity.getActivity();
+
+        onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(0)
+                .perform(click());
+
+    }
+
+    @Test
+    public void updateContact() throws Exception {
+
+    }
+
+    @Test
+    public void deleteContact() throws Exception {
+
+    }
 }
